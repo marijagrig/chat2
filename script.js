02c8e27bc06a3078250eebe4db4473986a9f8ed2
marijagrig <92846864+marijagrig@users.noreply.github.com>
2022-03-18 15:27:45 +0200

    4

--- script.js
+++ script.js
@@ -9,13 +9,20 @@ function sutitZinu()
     console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+    fetch(API + '/sutit/' + vards.value + '/' + zina.value)
 }
 
 async function ieladetChataZinas()
 {
-    let datiNoServera = await fetch('chatazinas.txt');
+    let datiNoServera = await fetch(API + '/lasit');
     let dati = await datiNoServera.text();
     zinas.innerHTML = dati;
 }
 
+async function ieladetChataZinasJson()
+{
+    let datiNoServera = await fetch(API + '/lasit');
+    let dati = await datiNoServera.json();
+    console.log (await dati)
+}
 setInterval(ieladetChataZinas, 1000)
