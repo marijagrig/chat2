d41ef9660709ec1cb6fc72feb393b4e2af6d3f2c
marijagrig <92846864+marijagrig@users.noreply.github.com>
2022-03-18 12:09:20 +0200

    2

---
+++ script.js
@@ -0,0 +1,19 @@
+//console.log(2)
+let zina = document.querySelector('.manaZina');
+let zinas = document.querySelector('.chataZinas');
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+}
+
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('chatazinas.txt');
+    let dati = await datiNoServera.text();
+    zinas.innerHTML = dati;
+}
+
+setInterval(ieladetChataZinas)
